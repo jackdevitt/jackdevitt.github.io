10358e2c811bc590050c04acd7e904ea0d1e61f5
jackdevitt <46631988+jackdevitt@users.noreply.github.com>
2020-03-24 13:16:15 -0400

    hello

--- JavascriptExecutorEXTENSION.js
+++ JavascriptExecutorEXTENSION.js
@@ -4,4 +4,10 @@ var Person = function (name, yearOfBirth, job) {
     this.job = job;
 }
 
+Person.prototype.calculateAge = function () {
+    console.log(2020 - this.yearOfBirth)
+}
+
 var john = new Person ('John', 1990, 'Teacher');
+
+john.calculateAge()
